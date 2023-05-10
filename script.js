d8ede613b0d0bb7152de8677d5c7adf3e9242129
pavandandu17 <pavandandu17@gmail.com>
2023-05-10 17:37:54 +0530

    Add functionality total sums and display todays sum

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const localStorageKey = 'expensesTrackerAppData';
 
+window.onload = showCurrMonthTotalExpense;
+window.addEventListener('storage', showCurrMonthTotalExpense);
+
+let today = new Date();
+
+function showCurrMonthTotalExpense() {
+    let temp = document.getElementById('todaysExpense');
+    const appData = JSON.parse(localStorage.getItem(localStorageKey));
+    temp.innerText = (appData.expenses[today.getFullYear()][today.getMonth()][today.getDate()].totalSum) + "/- Rs";
+    temp = document.getElementById('thisMonthsExpense');
+    temp.innerText = (appData.expenses[today.getFullYear()][today.getMonth()].totalSum) + "/- Rs";
+}
+
 function submitData() {
+    event.preventDefault();
     let expenseType = document.getElementById('expenseType').value;
     let expenseDescription = document.getElementById('expenseDescription').value;
     let expenseAmount = Number(document.getElementById('expenseAmount').value);
@@ -29,6 +43,7 @@ function submitData() {
 
 function addExpense(expenseData) {
     let appData = JSON.parse(localStorage.getItem(localStorageKey));
+
     let year = Number(expenseData.expenseDate.getFullYear());
     let monthIndex = Number(expenseData.expenseDate.getMonth());
     let date = Number(expenseData.expenseDate.getDate());
@@ -36,39 +51,35 @@ function addExpense(expenseData) {
     let expenses = appData.expenses;
     //Adding year if not present
     if(!expenses.hasOwnProperty(year)) {
-        expenses[year] = {}
+        expenses[year] = {
+            totalSum:0
+        };
     }
     //Adding month if not present
     if(!expenses[year].hasOwnProperty(monthIndex)) {
-        expenses[year][monthIndex] = {}
+        expenses[year][monthIndex] = {
+            totalSum:0
+        };
     }
     //Adding date if not present
     if(!expenses[year][monthIndex].hasOwnProperty(date)) {
-        expenses[year][monthIndex][date] = []
+        expenses[year][monthIndex][date] = {
+            totalSum: 0,
+            list:[]
+        };
     }
     
     //Adding current expense data
-    expenses[year][monthIndex][date].push(expenseData);
+    expenses[year][monthIndex][date].list.push(expenseData);
+
+    //Updating total Sums
+    expenses[year][monthIndex][date].totalSum += expenseData.expenseAmount;
+    expenses[year][monthIndex].totalSum += expenseData.expenseAmount;
+    expenses[year].totalSum += expenseData.expenseAmount;
+
     localStorage.setItem(localStorageKey, JSON.stringify(appData));
-}
 
-function displayExpenses() {
-
-    const data = JSON.parse(localStorage.getItem(localStorageKey));
-    if(data == null)
-        return;
-    const ul = document.getElementById('displayList');
-    ul.innerHTML = '';
-    for(expense of data)
-    {
-        let x = document.createElement('li');
-        x.innerHTML = JSON.stringify(expense);
-        
-        ul.appendChild(x);
-    }
-
-    document.getElementById('displayList').style.display = "block";
-    document.getElementById('displayList').style.transition = "0.5s";
+    showCurrMonthTotalExpense();
 }
 
 function clearData() {
